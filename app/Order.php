9ed136a0691f3ec899278e8a6233a73ec25d7edd
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-03-24 10:12:08 +0100

    done orders seeder

--- app/Order.php
+++ app/Order.php
@@ -6,6 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    protected $fillable = [
+        'customer_name',
+        'cutomer_email',
+        'total_amount'
+    ];
+
     // Relazione Many to Many con Food
 
     public function foods(){
