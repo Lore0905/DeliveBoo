d91030b2b8135f1a5c08a93244e36072189bc47b
ArturoTarantino <arturotarantino92@gmail.com>
2022-03-24 16:31:43 +0100

    edit dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,11 +28,6 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-
-                {{-- link create --}}
-                <a href="{{ route('admin.restaurant.create') }}">
-                    Inserisci il tuo ristorante
-                </a>
                 
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
