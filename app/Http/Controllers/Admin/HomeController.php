0a4201a891e500a1679f6e6a5c8a77337a2379f6
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-03-24 09:42:02 +0100

    fixed problems

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,13 +3,17 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use App\Restaurant;
+use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('admin.home');
+
+        $user = Auth::user();
+
+        return view('admin.home', compact('user'));
     }
 }
