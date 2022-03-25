fd44340c8bcc8a2bb9ff9036259955e1569e51fa
lorenzotaccetti <lorenzo.taccetti2001@gmail.com>
2022-03-25 14:03:04 +0100

    Fixed orders table

--- config/orders.php
+++ config/orders.php
@@ -3,26 +3,36 @@
         [
             "customer_name" => "Mario Rossi",
             "customer_email" => "mariorossi@mail.com",
+            "customer_address" => "Via Roma, 43 Firenze",
+            "customer_phone_number" => "3451627893",
             "total_amount" => 50
         ],
         [
             "customer_name" => "Michele Bianchi",
             "customer_email" => "michelebianchi@mail.com",
+            "customer_address" => "Via Pisana, 43 Perugia",
+            "customer_phone_number" => "3902561789",
             "total_amount" => 33
         ],
         [
             "customer_name" => "Federico Rossi",
             "customer_email" => "federicorossi@mail.com",
+            "customer_address" => "Via Livornese, 3 Terni",
+            "customer_phone_number" => "3127835455",
             "total_amount" => 44.67
         ],
         [
             "customer_name" => "Giovanni Viola",
             "customer_email" => "giovanniviola@mail.com",
+            "customer_address" => "Via Mantovana, 21 Capalbio",
+            "customer_phone_number" => "3339876245",
             "total_amount" => 22.5
         ],
         [
             "customer_name" => "Laura Verdi",
             "customer_email" => "lauraverdi@mail.com",
+            "customer_address" => "Via Ragusa, 43 Palermo",
+            "customer_phone_number" => "3352617829",
             "total_amount" => 45.03
         ],
     ];
