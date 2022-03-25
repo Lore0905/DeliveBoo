1c156642b4484383a5e33bcd26965d87f25f7f12
XanderGale <alessandro.galeazzi@icloud.com>
2022-03-25 04:08:44 +0100

    fixed show restaurant + forbidden->controller

--- resources/views/admin/restaurant/show.blade.php
+++ resources/views/admin/restaurant/show.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.login-dashboard')
 
-@section('content')
+{{-- @section('content')
     <div class="container">
         <h1>{{$restaurant_to_show->name}}</h1>
         <img src="{{ asset('storage/' . $restaurant_to_show->image ) }}" alt="{{ $restaurant_to_show->name . 'image' }}">
@@ -16,5 +16,46 @@
                 @endif
             @endforeach
         </div>
+    </div>
+@endsection --}}
+
+@section('content')
+    <div class="container">
+        <section>
+            <h3 style="margin: 10px">Il tuo ristorante:</h3>
+            <div class="row">
+
+                <div class="card">
+                    <img src="{{ asset('storage/' . $restaurant_to_show->image ) }}" alt="{{ $restaurant_to_show->name . ' image' }}">
+                    <div class="card-body">
+                      <h3 class="card-title">{{$restaurant_to_show->name}}</h5>
+                      <p class="card-text">Some description of the restaurant?</p>
+                    </div>
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item">
+                            <p style="font-weight: bolder" class="d-inline-block">Indirizzo:</p>
+                            <div>
+                                <p class="card-text">{{$restaurant_to_show->address}}</p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <p style="font-weight: bolder" class="d-inline-block">Numero di telefono:</p>
+                            <div>
+                                <p class="card-text">{{$restaurant_to_show->phone_number}}</p>
+                            </div>
+                        </li>
+                        <li class="list-group-item">
+                            <p style="font-weight: bolder" class="d-inline-block">Tipologie:</p>
+                            <div>
+                                @foreach ($restaurant_to_show->types as $type)
+                                    <span class="badge badge-light" >{{ $type->name }}</span>
+                                @endforeach     
+                            </div>
+                        </li>
+                    </ul>
+                  </div>
+            
+            </div>
+        </section>
     </div>
 @endsection
