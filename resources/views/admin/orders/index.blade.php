fbfc604a30450f745573a6c91d8d0ce93dfd99ee
ArturoTarantino <arturotarantino92@gmail.com>
2022-03-25 14:13:30 +0100

    edit orders index

--- resources/views/admin/orders/index.blade.php
+++ resources/views/admin/orders/index.blade.php
@@ -5,17 +5,27 @@
         Ordini
     </h1>
 
-    <div class="d-flex align-items-center">
+    <div class="d-flex align-items-center p-2">
         @foreach ($orders as $order)
-            <div class="border">
-                <div>
+            <div class="border mx-2 p-2">
+                <div class="mb-2 text-center font-weight-bold">
                     {{$order->customer_name}}
                 </div>
-                <div>
+                <div class="mb-2">
+                    <span class="font-weight-bold">Indirizzo:</span>
+                    {{$order->customer_address}}
+                </div>
+                <div class="mb-2">
+                    <span class="font-weight-bold">Telefono:</span>
+                    {{$order->customer_phone_number}}
+                </div>
+                <div class="mb-2">
+                    <span class="font-weight-bold">Email:</span>
                     {{$order->customer_email}}
                 </div>
-                <div>
-                    {{$order->total_amount}}$
+                <div class="mb-2">
+                    <span class="font-weight-bold">Totale:</span>
+                    {{$order->total_amount}}€
                 </div>
             </div>
         @endforeach
