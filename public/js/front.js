ee43843f5635b03ae918b6d39670559a84282285
LorenzoGirinelli <girinellilorenzo22@hotmail.com>
2022-03-31 17:07:44 +0200

    dipendenze

--- public/js/front.js
+++ public/js/front.js
@@ -22104,7 +22104,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\artur\repository\laravel-projects\DeliveBoo\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\loren\classe-48\DeliveBoo\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
