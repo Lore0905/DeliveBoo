9d47aa3f7da16c0f4919841bc8119620f5b5904a
Lore0905 <lore.castelli4@gmail.com>
2022-04-28 14:29:33 +0200

    modifiche generali

--- resources/views/admin/foods/show.blade.php
+++ resources/views/admin/foods/show.blade.php
@@ -11,60 +11,70 @@
 
             <div class="single-food">
 
-                {{-- Nome del piatto --}}
-                <h1 class="food-name">
-                    {{$food->name}}
-                </h1>
+                <div class="row row-cols-1 row-cols-lg-2">
 
-                {{-- Slug --}}
-                <div class="slug">
-                    <span class="left-column">
-                        Slug:
-                    </span>
-                    {{$food->slug}}
-                </div>
+                    <div class="col">
+                        {{-- Nome del piatto --}}
+                        <h1 class="food-name">
+                            {{$food->name}}
+                        </h1>
 
-                {{-- Descrizione --}}
-                <div class="description">
-                    <span class="left-column">
-                        Descrizione:
-                    </span>
-                    {{$food->descriptions}}
-                </div>
+                        {{-- Slug --}}
+                        <div class="slug">
+                            <span class="left-column">
+                                Slug:
+                            </span>
+                            {{$food->slug}}
+                        </div>
 
-                {{-- Ingredienti --}}
-                <div class="ingrediants">
-                    <span class="left-column">
-                        <i class="fas fa-hamburger"></i>
-                        Ingredienti:
-                    </span>
-                    {{$food->ingrediants}}
-                </div>
+                        {{-- Descrizione --}}
+                        <div class="description">
+                            <span class="left-column">
+                                Descrizione:
+                            </span>
+                            {{$food->descriptions}}
+                        </div>
 
-                {{-- Prezzo --}}
-                <div class="price">
-                    <span class="left-column">
-                        <i class="fas fa-money-bill-alt"></i>
-                        Prezzo:
-                    </span>
-                    {{$food->price}}
-                </div>
+                        {{-- Ingredienti --}}
+                        <div class="ingrediants">
+                            <span class="left-column">
+                                <i class="fas fa-hamburger"></i>
+                                Ingredienti:
+                            </span>
+                            {{$food->ingrediants}}
+                        </div>
 
-                {{-- Immagine --}}
-                <div class="img">
-                    <div class="left-column">
-                        <i class="far fa-image"></i>
-                        Copertina:
+                        {{-- Prezzo --}}
+                        <div class="price">
+                            <span class="left-column">
+                                <i class="fas fa-money-bill-alt"></i>
+                                Prezzo:
+                            </span>
+                            {{$food->price}}
+                        </div>
                     </div>
-                    <img src="{{asset('storage/' . $food->img)}}" alt="{{$food->name}}">
+
+                    <div class="col">
+                        {{-- Immagine --}}
+                        <div class="img">
+                            <div class="left-column">
+                                <i class="far fa-image"></i>
+                                Copertina:
+                            </div>
+                            <img src="{{asset('storage/' . $food->img)}}" alt="{{$food->name}}">
+                        </div>
+                    </div>
+
                 </div>
+
+                
 
                 {{-- Link --}}
-                <div class="d-flex align-items-center justify-content-between">
+                <div class="d-flex align-items-center ">
 
                     {{-- Edit --}}
                     <div>
-                        <a class="btn m_button" href="{{ route('admin.foods.edit', ['food' => $food->id]) }}">Modifica piatto</a>
+                        <a class="btn m_button m-b-992" href="{{ route('admin.foods.edit', ['food' => $food->id]) }}">Modifica piatto</a>
                     </div>
                     {{-- Delete --}}
 
@@ -72,7 +82,7 @@
                         @csrf
                         @method('DELETE')
             
-                        <button class="btn btn-danger" onclick="return confirm('Sei sicuro di voler eliminare il piatto?')">Elimina</button>
+                        <button class="btn destroy-button-show-food btn-danger mx-4 m-b-992" onclick="return confirm('Sei sicuro di voler eliminare il piatto?')">Elimina</button>
                     </form>
                 </div>
 
